fcc21e7a27a9fd464f5995388766855a662adacf
刘博 <liubo1@gg.com>
2024-01-03 19:27:25 +0800

    [修改] 云数据库地址

--- server/config.py
+++ server/config.py
@@ -1,2 +1,5 @@
 class Config:
-    MONGODB_URI = "mongodb+srv://liubo:abc123!@cluster1.cbbd34h.mongodb.net/treehole?authSource=admin&retryWrites=true&w=majority"
+    # 云数据库
+    # MONGODB_URI = "mongodb+srv://liubo:abc123!@cluster1.cbbd34h.mongodb.net/treehole?authSource=admin&retryWrites=true&w=majority"
+    # 本地服务器数据库
+    MONGODB_URI = "mongodb://root:123456@127.0.0.1:27017/treehole?authSource=admin&retryWrites=true&w=majority"
